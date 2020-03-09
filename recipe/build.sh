0d309cd0dfa3df9f4daf12cfadbdec2d864f5bd0
Tommaso Sartor <tmmsartor@gmail.com>
2020-03-09 17:43:58 +0100

    conda: Add metis and iomp5  as conda requirements

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,28 +2,17 @@
 
 export MINICONDA_LIB=${HOME}/.local/opt/miniconda/lib
 export MINICONDA_INCLUDE=${HOME}/.local/opt/miniconda/include
+export CMAKE_BUILD_TYPE=Release
 
 curdir=`pwd`
+builddir=$curdir/build/$CMAKE_BUILD_TYPE
 
-#Build direcetories
+# Build direcetories
 mkdir -p build
-mkdir -p build/debug
+mkdir -p build/$CMAKE_BUILD_TYPE
 mkdir -p build/lib
-mkdir -p build/metis
 
-metisdir=$curdir/build/metis
-cd $metisdir
 
-cmake $curdir/suitesparse/metis-5.1.0 -DGKLIB_PATH=$curdir/suitesparse/metis-5.1.0/GKlib -DSHARED=1 && make
-cd $curdir
-cp build/metis/libmetis/libmetis.so build/lib/
-
-#Build QPALM and tests
-cd $curdir
-
-builddir=$curdir/build/debug
-
-cd $builddir
-
-cmake $curdir -DCMAKE_BUILD_TYPE=release -DCOVERAGE=ON
-make
+pushd $builddir
+cmake $curdir -DCMAKE_BUILD_TYPE=release -DCOVERAGE=ON && make
+popd
